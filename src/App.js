edfe063ac00130338e66b827161a19e7850e8a6b
jab_tx <javier.a.barragan92@gmail.com>
2020-05-29 16:20:39 -0500

    fixxx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Search from "./components/Search/search";
-import Footer from "./components/footer/footer";
+// import Footer from "./components/footer/footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Search />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
